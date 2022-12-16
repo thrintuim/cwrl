c2f31e209aa0122a731251d51294247ce2c0f42b
Thrintuim <douglas.arthur.chappell@gmail.com>
2022-12-16 16:57:24 -0500

    removed unused referrence to screen

--- cwrl-client/src/Board.test.js
+++ cwrl-client/src/Board.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Board from './Board'
 
 test('renders Board component without error', () => {
