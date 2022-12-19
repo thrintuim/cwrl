01587d81ae805631d2c200b6d1a39c2f451c31a3
Thrintuim <douglas.arthur.chappell@gmail.com>
2022-12-19 15:39:03 -0500

    whitespace adjustments to CWRL.js

--- cwrl-client/src/CWRL.js
+++ cwrl-client/src/CWRL.js
@@ -5,51 +5,56 @@ import Messages from './Messages'
 
 class CWRL extends React.Component {
     constructor(props) {
-	super(props)
-	this.state = {
-	    gameObjects: []
-	}
-	this.movementHandlers = {
-	    left: this.handleMove.bind(this, "left"),
-	    right: this.handleMove.bind(this, "right"),
-	    up: this.handleMove.bind(this, "up"),
-	    down: this.handleMove.bind(this, "down")
-	}
+		super(props)
+		this.state = {
+			gameObjects: []
+		}
+		this.movementHandlers = {
+			left: this.handleMove.bind(this, "left"),
+			right: this.handleMove.bind(this, "right"),
+			up: this.handleMove.bind(this, "up"),
+			down: this.handleMove.bind(this, "down")
+		}
     }
 
     handleMove(direction) {
-	const activeObj = this.state.gameObjects.filter((el) => el.active)[0]
-	const inactiveObjects = this.state.gameObjects.filter(() => !el.active)
-	switch (direction) {
-	case "left":
-	    activeObj.x - 1
-	    break
-	case "right":
-	    activeObj.x + 1
-	    break
-	case "up":
-	    activeObj.y - 1
-	    break
-	case "down":
-	    activeObj.y + 1
-	    break
-	}
-	this.setState({
-	    gameObjects: [activeObj, ...inactiveObjects]
-	})
+		const activeObj = this.state.gameObjects.filter((el) => el.active)[0]
+		const inactiveObjects = this.state.gameObjects.filter((el) => !el.active)
+		switch (direction) {
+			case "left":
+				activeObj.x -= 1
+				break
+			case "right":
+				activeObj.x += 1
+				break
+			case "up":
+				activeObj.y -= 1
+				break
+			case "down":
+				activeObj.y += 1
+				break
+			default:
+				break
+		}
+		this.setState({
+			gameObjects: [activeObj, ...inactiveObjects]
+		})
     }
 
     addObjects(obj, index) {
-	return (
-	    <rect
-		key={`player${obj.player}`}
-		x={obj.x}
-		y={obj.y}
-		width="1"
-		height="4"
-		id={`player${obj.player}`}
-	    />
-	)
+		return (
+			<rect
+				key={`player${obj.player}`}
+				x={obj.x}
+				y={obj.y}
+				width="1"
+				height="4"
+				id={`player${obj.player}`}
+				fill="none"
+				strokeWidth="0.5"
+				stroke="black"
+			/>
+		)
     }
 
     render(props) {
@@ -60,14 +65,14 @@ class CWRL extends React.Component {
 			</header>
 			<main>
 			    <Board
-				objects={this.gameObjects}
-				add={this.addObjects}
+					objects={this.gameObjects}
+					add={this.addObjects}
 			    />
 			    <Controls handlers={this.movementHandlers}/>
 			    <Messages
-				title={"Movement History"}
-				level={2}
-				id={"movementHistory"}
+					title={"Movement History"}
+					level={2}
+					id={"movementHistory"}
 			    />
 			</main>
 			</>
