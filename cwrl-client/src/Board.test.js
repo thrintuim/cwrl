1a9ac895b1880acb8630ec1c1858488d7ac57259
Thrintuim <douglas.arthur.chappell@gmail.com>
2022-12-23 14:05:15 -0500

    Simplified Board component and now adjusted test.

--- cwrl-client/src/Board.test.js
+++ cwrl-client/src/Board.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Board from './Board'
 
 test('renders Board component without error', () => {
@@ -8,19 +8,8 @@ test('renders Board component without error', () => {
   expect(renderBoard).not.toThrow()
 });
 
-test('renders Board component with objects', () => {
-    function addObjects(obj, index) {
-        return (<rect key={index} x={obj.x} y={obj.y} height="4" width="1" className="player" id={`player${index}`} />)
-    } 
-    render(<Board objects={[{x: 25, y: 35}, {x: 50, y: 75}]} add={addObjects} />)
-    expect(document.getElementsByClassName("player").length).toBe(2)
+test('renders Board component with children', () => {
+    render(<Board><p>this is a thing</p></Board>)
+    expect(screen.getByText('this is a thing')).toBeDefined()
 })
 
-test('throws an error when board is sent non objects as object props', () => {
-    let cn = console.error
-    console.error = jest.fn()
-    expect(() => {
-        render(<Board objects={1}/>);
-    }).toThrow(TypeError);
-    console.error = cn
-  });
