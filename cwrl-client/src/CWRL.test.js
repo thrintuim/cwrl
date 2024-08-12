5c2700ecede598e13d87d2a1ed3d2e481f6dab0a
Thrintuim <douglas.arthur.chappell@gmail.com>
2024-08-12 14:25:15 -0400

    feat: trivial test for player identification

--- cwrl-client/src/CWRL.test.js
+++ cwrl-client/src/CWRL.test.js
@@ -1,9 +1,47 @@
 import { render, screen } from '@testing-library/react';
 import CWRL from './CWRL';
+import WebSocket from 'ws';
+
+jest.mock('ws')
+
+let mockConnection
+beforeEach (() => {
+  mockConnection = new WebSocket()
+})
+afterEach(() => {
+  jest.resetAllMocks()
+})
 
 test('renders CWRL app without error', () => {
   function renderCWRL() {
-    render(<CWRL />);
+    render(<CWRL serverConnection={mockConnection} />);
   }
   expect(renderCWRL).not.toThrow()
 });
+
+test('player role and number is present', async () => {
+
+  let eventHandler= [];
+  // TO DO: Need to move WebSocket to App and pass values to CWRL
+  mockConnection.addEventListener.mockImplementation((event, handler) => {
+    eventHandler[event] = handler;
+  });
+  mockConnection.close.mockImplementation(() => {})
+  // expected player data
+  let data = [{
+    active: true,
+    player: 1,
+    x: 50,
+    y: 0
+  },
+  {
+    active: false,
+    player: 2,
+    x: 0,
+    y: 50
+  }]
+  render(<CWRL serverConnection={mockConnection} />)
+  mockConnection.emit('message', JSON.stringify(data))
+  // CWRL - (player|observer) \d+
+  expect(await screen.findByText('player 1')).toBeInTheDocument()
+})
